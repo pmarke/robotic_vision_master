09b1ccabbb3d099fc739fe996fa17569ff6d341b
Mark Petersen <markiepetersen@gmail.com>
2018-01-18 19:22:46 -0700

    added parameters to LK_Matcher

--- src/rvm/include/rvm/feature_manager/lk_matcher.h
+++ src/rvm/include/rvm/feature_manager/lk_matcher.h
@@ -5,7 +5,7 @@
 
 namespace robotic_vision{
 
-	enum Feature_Detector_Type ={GFTT FAST};
+	enum LK_Feature_Detector_Type ={GFTT FAST};
 
 	class LK_Matcher : FeatureMatcher 
 	{
@@ -15,21 +15,30 @@ namespace robotic_vision{
 		virtual void find_correspoinding_features(const cv::Mat& img, std::vector<cv::Point2f>& prev_features, std::vector<cv::Point2f>& new_matched, const cv::Mat& mask);
 
 	private:
-		std::vector<cv::Point2f> prev_features_;
+		std::vector<cv::Point2f> prev_features_ = NULL;
+
+		LK_Feature_Detector_Type feature_detector_type_;
+
+		cv::Mat prev_image_;
 
 
 		// gftt algorithm
 		cv::Ptr<cv::GFTTDetector> gftt;
-		struct Gftt_Parameters{
-			int maxCorners;
-			double qualityLevel;
-			double minDistance;
-			double blockSize;
-			bool useHarrisDetector;
+		struct GFTT_Parameters{
+			int maxCorners;          // Max number of features that will be detected
+			double qualityLevel;     // Minimal accepted quality of image corners
+			double minDistance;      // Minimum possible Euclidean distance between the returned corners
+			double blockSize;        // Neighborhood size: block_size X block_size (pixels)
+			bool useHarrisDetector;  // Harris detector free parameter
 			double k;
-		}gftt_parameters;
+		}gftt_parameters_;
 
 		// fast parameters
+		struct FAST_Parameters{
+			int threshold;           // Threshold difference between intensity of the central pixel and the pixels of a circle around this pixel [0,255]
+			bool nonmaxSuppression;  // If true, non-maximum suppression is applied to keypoints (features)
+			int type;                // One of the three neighbors: FastFeatureDetector::TYPE_9_16, FastFeatureDetector::TYPE_7_12, FastFeatureDetector::TYPE_5_8
+		} fast_parameters_;
 		
 
 		// find features using GFTT algorithm
--- src/rvm/src/feature_manager/lk_matcher.cpp
+++ src/rvm/src/feature_manager/lk_matcher.cpp
@@ -4,9 +4,41 @@ namespace robotic_vision{
 
 	LK_Matcher::LK_Matcher(){
 
+		ros::NodeHandle nh("~");
+
+		// get parameters
+		nh.param("gftt_maxCorners", gftt_parameters.maxCorners, 500);
+		nh.param("gftt_qualityLevel", gftt_parameters.qualityLevel, 0.05);
+		nh.param("gftt_minDistance", gftt_parameters.minDistance, 10);
+		nh.param("gftt_blockSize", gftt_parameters.blockSize, 9);
+		nh.param("gftt_useHarrisDetector", gftt_parameters.gftt_useHarrisDetector, true);
+		nh.param("gftt_k", gftt_parameters.k, 0.05);
+
+		nh.param("fast_threshold", fast_parameters.threshold,100 );
+		nh.param("fast_nonmaxSuppression", fast_parameters.nonmaxSuppression, true );
+		nh.param("fast_type", fast_parameters.type, cv::FastFeatureDetector::TYPE_9_16);
+
+		nh.param("LK_Feature_Detector_Type", feature_detector_type_, GFTT);
+
+		// Create gftt object with given parameters
+		gftt.create(gftt_parameters.maxCorners,gftt_parameters.qualityLevel,gftt_parameters.minDistance,gftt_parameters.blockSize, gftt_parameters.gftt_useHarrisDetector,gftt_parameters.k);
 	}
 
+	virtual void LK_Matcher::find_correspoinding_features(const cv::Mat& img, std::vector<cv::Point2f>& prev_features, std::vector<cv::Point2f>& new_matched, const cv::Mat& mask){
 
+		// First Image
+		if(prev_features_ == NULL)
+		{
+			// Get features from the image
+			(feature_detector_type_ == GFTT) ? detect_features_GFTT(img, prev_features_) : detect_features_FAST(prev_features_);
+
+			prev_image_ = img;
+		}
+		else // There has been at least one image processed
+		{
+			cv::calcOpticalFlowPyrLK(prev_image_, img, prev_features_, new_matched, status, err, cv::Size(21,21),3,0,0.0001);
+		}
+	}
 
 	void LK_Matcher::detect_features_GFTT(const cv::Mat& img, std::vector<cv::Point2f>& features){
 
@@ -24,8 +56,10 @@ namespace robotic_vision{
 		features.clear();
 
 		// get the features
-		cv::FAST(img,features,FAST_threshold_, FAST_nonmaxSupression_);
+		cv::FAST(img,features,fast_parameters_.threshold, fast_parameters_.nonmaxSuppression, fast_parameters_.type);
 	}
+
+
 
 
 }
